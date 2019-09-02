3feed03c5b7b45f5ff320b1075f13836936b726a
aland1013 <adam.land@gmail.com>
2019-09-02 07:34:26 -0400

    through 49

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirectoryPath));
 
@@ -28,7 +31,8 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page',
-        name: 'Adam Land'
+        name: 'Adam Land',
+        helpText: 'This is some helpful text'
     });
 });
 
