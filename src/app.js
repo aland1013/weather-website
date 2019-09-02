cd09f7dfed03dc6940f3a7b7e5625383db58a941
aland1013 <adam.land@gmail.com>
2019-09-02 07:55:56 -0400

    througn 50

--- src/app.js
+++ src/app.js
@@ -43,6 +43,22 @@ app.get('/weather', (req, res) => {
     });
 });
 
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404 Error',
+        name: 'Adam Land',
+        errorMessage: 'Help article not found'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404 Error',
+        name: 'Adam Land',
+        errorMessage: 'Page not found'
+    });
+});
+
 app.listen(3000, () => {
     console.log('Server is up on port 3000');
 });
